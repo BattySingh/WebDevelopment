8f11be7a5cfe7831b594843ebfaf19a6261aa9e8
BattySingh <singhbatty02@gmail.com>
2019-08-11 19:52:25 +0530

    Learned about data types in c-sharp

---
+++ cSharp/exercise1/exercise1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    class Program
+    {
+        const string myBirthDate = "3-02-1992";
+
+        static void Main(string[] args)
+        {
+            byte byte1 = 225;
+            sbyte byte2 = 125;
+            int int1 = 3;
+            uint int2 = 3232423423;
+            short short1 = 22341;
+            ushort short2 = 25423;
+            long long1 = 2342342423432;
+            ulong long2 = 3432332342332423432;
+            float float1 = 23423.324324234234234f;
+            double double1 = 324323223.3243243242343343123432432;
+            char char1 = 's';
+            bool bool1 = true;
+            string string1 = "Son Goku";
+            decimal decimal1 = 243.3423432423423432432432423432423423M;
+
+            Console.WriteLine("Byte: " + byte1);
+            Console.WriteLine("sByte: " + byte2);
+            Console.WriteLine("Int: " + int1);
+            Console.WriteLine("uInt: " + int2);
+            Console.WriteLine("Short: " + short1);
+            Console.WriteLine("uShort: " + short2);
+            Console.WriteLine("Long: " + long1);
+            Console.WriteLine("uLong: " + long2);
+            Console.WriteLine("Float: " + float1);
+            Console.WriteLine("Double: " + double1);
+            Console.WriteLine("Char: " + char1);
+            Console.WriteLine("Bool: " + bool1);
+            Console.WriteLine("String: " + string1);
+            Console.WriteLine("Decimal: " + decimal1);
+
+            string myString1 = "I control text";
+            string myString2 = "32";
+            int myInt = Int32.Parse(myString2);
+
+            Console.WriteLine("String to Integer: " + myInt);
+
+            Console.WriteLine("My birthdate: {0}", myBirthDate);
+
+            Console.Read();
+        }
+    }
+}
---
+++ cSharp/typeConversion/typeConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double myDouble = 4.323;
+            float myFloat = 1.2f;
+
+            int num1 = (int)myDouble;
+
+            Console.WriteLine("Converted to int: " + num1);
+
+            string myString = myFloat.ToString();
+
+            Console.WriteLine("Float Converted to String: " + myString);
+
+            bool sunShining = true;
+            Console.WriteLine("Sun is shining: " + sunShining);
+
+            string text1 = "15";
+            string text2 = "13";
+            string myResult = text1 + text2;
+
+            Console.WriteLine("The string result: " + myResult);
+
+            int intText1 = Int32.Parse(text1);
+            int intText2 = Int32.Parse(text2);
+
+            int intResult = intText1 + intText2;
+            Console.WriteLine("Integer Result: " + intResult);
+
+            Console.Read();
+        }
+    }
+}
